ce7403d23f49a34c3b4ca9bbfc4978b8859d4925
B1azz <blazher4@gmail.com>
2020-01-05 09:09:32 +0300

    fix test

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_spanishLocalization(browser):
+def test_spanish_localization(browser):
     browser.get(link)
     time.sleep(30)
     button = WebDriverWait(browser, 5)\
