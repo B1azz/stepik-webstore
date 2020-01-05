09db64b26f551757a623c60f9fbd6dd83f7475be
B1azz <blazher4@gmail.com>
2020-01-05 09:02:29 +0300

    create test

---
+++ test_items.py
@@ -0,0 +1,15 @@
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_spanishLocalization(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = WebDriverWait(browser, 5)\
+        .until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket")))
+    assert button is not None, "Button not found"
